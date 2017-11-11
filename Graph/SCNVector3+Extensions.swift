d73d406578f36663b942cd4d499cc28018236e8a
Fraser King <frasertheking@gmail.com>
2017-11-11 18:14:08 -0500

    Added ability to draw lines between random points

---
+++ Graph/SCNVector3+Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  SCNVector3+Extensions.swift
+//  Graph
+//
+//  Created by Fraser King on 2017-11-11.
+//  Copyright © 2017 Fraser King. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+extension SCNVector3 {
+    static func subtract(a: SCNVector3, b: SCNVector3) -> SCNVector3 {
+        return SCNVector3(x: a.x - b.x, y: a.y - b.y, z: a.z - b.z)
+    }
+    
+    static func cross(a: SCNVector3, b: SCNVector3) -> SCNVector3 {
+        return SCNVector3(x: (a.y*b.z - a.z*b.y), y: (a.z*b.x - a.x*b.z), z: (a.x*b.y - a.y*b.x))
+    }
+    
+    static func dot(a: SCNVector3, b: SCNVector3) -> Double {
+        return Double(a.x*b.x + a.y*b.y + a.z*b.z)
+    }
+    
+    func length() -> Double {
+        return Double(sqrtf(x*x + y*y + z*z))
+    }
+    
+    func distance(receiver:SCNVector3) -> Float {
+        let xd = receiver.x - self.x
+        let yd = receiver.y - self.y
+        let zd = receiver.z - self.z
+        let distance = Float(sqrt(xd * xd + yd * yd + zd * zd))
+
+        if (distance < 0){
+            return (distance * -1)
+        } else {
+            return (distance)
+        }
+    }
+}
