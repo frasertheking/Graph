bcb8a0cac0a91addfe9eede7ed70b05f84e9d227
Fraser King <frasertheking@gmail.com>
2017-12-15 12:31:23 -0500

    Added another pod

--- Graph/GameViewController.swift
+++ Graph/GameViewController.swift
@@ -121,14 +121,11 @@ class GameViewController: UIViewController {
         // Custom Duration
         pastelView.animationDuration = 3.0
         
-        // Custom Color
-        pastelView.setColors([UIColor(red: 156/255, green: 39/255, blue: 176/255, alpha: 1.0),
-                              UIColor(red: 255/255, green: 64/255, blue: 129/255, alpha: 1.0),
-                              UIColor(red: 123/255, green: 31/255, blue: 162/255, alpha: 1.0),
-                              UIColor(red: 32/255, green: 76/255, blue: 255/255, alpha: 1.0),
-                              UIColor(red: 32/255, green: 158/255, blue: 255/255, alpha: 1.0),
-                              UIColor(red: 90/255, green: 120/255, blue: 127/255, alpha: 1.0),
-                              UIColor(red: 58/255, green: 255/255, blue: 217/255, alpha: 1.0)])
+        // Custom Color        
+        pastelView.setColors([UIColor(red: 247/255, green: 202/255, blue: 24/255, alpha: 1.0),
+                                UIColor(red: 255/255, green: 182/255, blue: 30/255, alpha: 1.0),
+                                UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1.0),
+                                UIColor(red: 245/255, green: 215/255, blue: 110/255, alpha: 1.0)])
         
         pastelView.startAnimation()
         
--- Podfile
+++ Podfile
@@ -2,3 +2,4 @@ use_frameworks!
 
 target 'Graph'
 pod "Pastel"
+pod 'M13Checkbox'
